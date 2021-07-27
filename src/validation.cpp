5f5884f193c43bada3f3d7f23a3cc437724c5f7d
glozow <gloriajzhao@gmail.com>
2021-07-27 14:23:40 +0100

    MOVEONLY: GetEntriesForRBF to policy/rbf

--- src/validation.cpp
+++ src/validation.cpp
@@ -884,31 +884,9 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
             }
         }
 
-        std::set<uint256> setConflictsParents;
-        uint64_t nConflictingCount = 0;
-        for (const auto& mi : setIterConflicting) {
-            for (const CTxIn &txin : mi->GetTx().vin)
-            {
-                setConflictsParents.insert(txin.prevout.hash);
-            }
+        // Calculate all conflicting entries and enforce Rules 2 and 5.
+        if (!GetEntriesForRBF(tx, m_pool, setIterConflicting, state, allConflicting)) return false;
 
-            nConflictingCount += mi->GetCountWithDescendants();
-            // This potentially overestimates the number of actual descendants
-            // but we just want to be conservative to avoid doing too much
-            // work.
-            if (nConflictingCount > MAX_BIP125_REPLACEMENT_CANDIDATES) {
-                return state.Invalid(TxValidationResult::TX_MEMPOOL_POLICY, "too many potential replacements",
-                        strprintf("rejecting replacement %s; too many potential replacements (%d > %d)\n",
-                            hash.ToString(),
-                            nConflictingCount,
-                            MAX_BIP125_REPLACEMENT_CANDIDATES));
-            }
-        }
-        // If not too many to replace, then calculate the set of
-        // transactions that would have to be evicted
-        for (CTxMemPool::txiter it : setIterConflicting) {
-            m_pool.CalculateDescendants(it, allConflicting);
-        }
         // Check if it's economically rational to mine this transaction rather
         // than the ones it replaces.
         ws.m_conflicting_fees = 0;
@@ -918,29 +896,6 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
             ws.m_conflicting_size += it->GetTxSize();
         }
 
-        for (unsigned int j = 0; j < tx.vin.size(); j++)
-        {
-            // We don't want to accept replacements that require low
-            // feerate junk to be mined first. Ideally we'd keep track of
-            // the ancestor feerates and make the decision based on that,
-            // but for now requiring all new inputs to be confirmed works.
-            //
-            // Note that if you relax this to make RBF a little more useful,
-            // this may break the CalculateMempoolAncestors RBF relaxation,
-            // above. See the comment above the first CalculateMempoolAncestors
-            // call for more info.
-            if (!setConflictsParents.count(tx.vin[j].prevout.hash))
-            {
-                // Rather than check the UTXO set - potentially expensive -
-                // it's cheaper to just check if the new input refers to a
-                // tx that's in the mempool.
-                if (m_pool.exists(tx.vin[j].prevout.hash)) {
-                    return state.Invalid(TxValidationResult::TX_MEMPOOL_POLICY, "replacement-adds-unconfirmed",
-                            strprintf("replacement %s adds unconfirmed input, idx %d",
-                                hash.ToString(), j));
-                }
-            }
-        }
 
         // The replacement must pay greater fees than the transactions it
         // replaces - if we did the bandwidth used by those conflicting
