cf47c9528e1b9c93cd07639c295735bdefe3ac79
glozow <gloriajzhao@gmail.com>
2021-07-27 17:37:10 +0100

    scripted-diff: variable names in policy/rbf
    Fee Delta is a term used for prioritizing transactions (difference
    between base and modified fees). Here, we are referring to the
    (modified) fee difference between transactions in the mempool and the
    replacement candidates, and want to know the additional fees we would
    get from accepting the new transactions. It's needlessly confusing to
    overload this term.
    
    The rest are just style changes - using snake_case instead of camelCase.
    
    -BEGIN VERIFY SCRIPT-
    
    ren() { sed -i "s/\<$1\>/$2/g" src/policy/rbf* ; }
    
    ren setAncestors ancestors
    ren setIterConflicting conflict_iterators
    ren setConflicts set_conflicts
    ren nSize replacement_vsize
    ren m_pool pool
    ren newFeeRate replacement_feerate
    ren oldFeeRate original_feerate
    ren allConflicting all_conflicts
    ren setConflicts_parents set_conflict_parents
    
    ren nDeltaFees additional_fees
    
    -END VERIFY SCRIPT-

--- src/policy/rbf.h
+++ src/policy/rbf.h
@@ -42,45 +42,45 @@ bool IsRBFOptOut(const CTransaction& txConflicting);
 
 /** Get all descendants of setIterConflicting. Also enforce BIP125 Rule 5 and quit as early as
  * possible. There cannot be more than MAX_BIP125_REPLACEMENT_CANDIDATES potential entries.
- * @param[out]  allConflicting      Populated with all the mempool entries that would be replaced,
- *                                  which includes descendants of setIterConflicting. Not cleared at
+ * @param[out]  all_conflicts      Populated with all the mempool entries that would be replaced,
+ *                                  which includes descendants of conflict_iterators. Not cleared at
  *                                  the start; any existing mempool entries will remain in the set.
  * @returns false if Rule 5 is broken.
  */
-bool GetEntriesForRBF(const CTransaction& tx, CTxMemPool& m_pool,
-                      const CTxMemPool::setEntries setIterConflicting, TxValidationState& state,
-                      CTxMemPool::setEntries& allConflicting) EXCLUSIVE_LOCKS_REQUIRED(m_pool.cs);
+bool GetEntriesForRBF(const CTransaction& tx, CTxMemPool& pool,
+                      const CTxMemPool::setEntries conflict_iterators, TxValidationState& state,
+                      CTxMemPool::setEntries& all_conflicts) EXCLUSIVE_LOCKS_REQUIRED(pool.cs);
 
 /** Enforce BIP125 Rule 2: a replacement transaction must not add any new unconfirmed inputs. */
-bool HasNoNewUnconfirmed(const CTransaction& tx, CTxMemPool& m_pool,
+bool HasNoNewUnconfirmed(const CTransaction& tx, CTxMemPool& pool,
                          const CTxMemPool::setEntries setIterConflicting,
-                         TxValidationState& state) EXCLUSIVE_LOCKS_REQUIRED(m_pool.cs);
+                         TxValidationState& state) EXCLUSIVE_LOCKS_REQUIRED(pool.cs);
 
 /** Check the intersection between original mempool transactions (candidates for being replaced) and
  * the ancestors of replacement transactions.
  * @param[in]   hash    Transaction ID, included in the error message if violation occurs.
  * returns false if the intersection is empty, true if otherwise.
  */
-bool SpendsAndConflictsDisjoint(CTxMemPool::setEntries& setAncestors, std::set<uint256> setConflicts,
+bool SpendsAndConflictsDisjoint(CTxMemPool::setEntries& ancestors, std::set<uint256> set_conflicts,
                                 TxValidationState& state, const uint256& hash);
 
 /** Check that the feerate of the replacement transaction(s) is higher than the feerate of each
- * of the transactions in setIterConflicting.
+ * of the transactions in conflict_iterators.
  */
-bool PaysMoreThanConflicts(const CTxMemPool::setEntries& setIterConflicting, CFeeRate newFeeRate,
+bool PaysMoreThanConflicts(const CTxMemPool::setEntries& conflict_iterators, CFeeRate replacement_feerate,
                            TxValidationState& state, const uint256& hash);
 
 /** Enforce BIP125 Rules 3 and 4 to ensure that replacement transaction fees are sufficient to
  * replace all conflicting mempool entries.
- * @param[in]   nConflictingFees    Total modified fees of original transaction(s).
- * @param[in]   nConflictingSize    Total virtual size of original transaction(s).
- * @param[in]   nModifiedFees       Total modified fees of replacement transaction(s).
- * @param[in]   nSize               Total virtual size of replacement transaction(s).
+ * @param[in]   conflict_fees    Total modified fees of original transaction(s).
+ * @param[in]   conflict_vsize    Total virtual size of original transaction(s).
+ * @param[in]   replacement_fees       Total modified fees of replacement transaction(s).
+ * @param[in]   replacement_vsize               Total virtual size of replacement transaction(s).
  * @param[in]   hash                Transaction ID, included in the error message if violation occurs.
  * returns true if fees are sufficient, false if otherwise.
  */
-bool PaysForRBF(CAmount nConflictingFees, size_t nConflictingSize,
-                CAmount nModifiedFees, size_t nSize,
+bool PaysForRBF(CAmount conflict_fees, size_t conflict_vsize,
+                CAmount replacement_fees, size_t replacement_vsize,
                 TxValidationState& state, const uint256& hash);
 
 #endif // BITCOIN_POLICY_RBF_H
