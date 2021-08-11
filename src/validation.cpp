3e8643875939bfc63fe09344f091f80f59fdc4a3
glozow <gloriajzhao@gmail.com>
2021-08-11 15:51:27 +0100

    MOVEONLY: check that fees > direct conflicts to policy/rbf

--- src/validation.cpp
+++ src/validation.cpp
@@ -771,31 +771,7 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
     if (fReplacementTransaction)
     {
         CFeeRate newFeeRate(nModifiedFees, nSize);
-        for (const auto& mi : setIterConflicting) {
-            // Don't allow the replacement to reduce the feerate of the
-            // mempool.
-            //
-            // We usually don't want to accept replacements with lower
-            // feerates than what they replaced as that would lower the
-            // feerate of the next block. Requiring that the feerate always
-            // be increased is also an easy-to-reason about way to prevent
-            // DoS attacks via replacements.
-            //
-            // We only consider the feerates of transactions being directly
-            // replaced, not their indirect descendants. While that does
-            // mean high feerate children are ignored when deciding whether
-            // or not to replace, we do require the replacement to pay more
-            // overall fees too, mitigating most cases.
-            CFeeRate oldFeeRate(mi->GetModifiedFee(), mi->GetTxSize());
-            if (newFeeRate <= oldFeeRate)
-            {
-                return state.Invalid(TxValidationResult::TX_MEMPOOL_POLICY, "insufficient fee",
-                        strprintf("rejecting replacement %s; new feerate %s <= old feerate %s",
-                            hash.ToString(),
-                            newFeeRate.ToString(),
-                            oldFeeRate.ToString()));
-            }
-        }
+        if (!PaysMoreThanConflicts(setIterConflicting, newFeeRate, state, hash)) return false;
 
         // Calculate all conflicting entries and enforce Rules 2 and 5.
         if (!GetEntriesForRBF(tx, m_pool, setIterConflicting, state, allConflicting)) return false;
