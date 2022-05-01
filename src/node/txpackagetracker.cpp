2edb5d411c8d4ed7ca03f5b38f6a8f6175f522d6
glozow <gloriajzhao@gmail.com>
2022-05-01 10:41:53 -0700

    [p2p] add tx package tracker

---
+++ src/node/txpackagetracker.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2022
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <node/txpackagetracker.h>
+
+namespace node {
+    /** How long to wait before requesting orphan ancpkginfo/parents from an additional peer.
+     * Same as GETDATA_TX_INTERVAL. */
+    static constexpr auto ORPHAN_ANCESTOR_GETDATA_INTERVAL{60s};
+class TxPackageTracker::Impl {
+};
+
+TxPackageTracker::TxPackageTracker() : m_impl{std::make_unique<TxPackageTracker::Impl>()} {}
+TxPackageTracker::~TxPackageTracker() = default;
+
+} // namespace node
