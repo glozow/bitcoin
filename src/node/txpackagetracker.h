50230127ce6f2295f1aeaaae4f697d9dc38a5f6f
glozow <gloriajzhao@gmail.com>
2022-05-01 10:41:53 -0700

    [refactor] add TxPackageTracker, wrapping TxOrphanage
    This module is going to be responsible for managing everything related
    to transaction packages, including orphan transactions and package
    relay. It will be responsible for managing usage of the TxOrphanage and
    instructing PeerManager:
    - what tx or package-related messages to send to which peer
    - whether a tx or package-related message is allowed or useful
    - what transactions are available to try accepting to mempool
    
    The orphanage changes are move-only. Reviewers can observe that the
    functions and comments are copied from txorphanage.h.
    Future commits will consolidate the interface and re-delegate some
    orphanage interactions from PeerManager to TxPackageTracker.

---
+++ src/node/txpackagetracker.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_NODE_TXPACKAGETRACKER_H
+#define BITCOIN_NODE_TXPACKAGETRACKER_H
+
+#include <net.h>
+
+#include <cstdint>
+#include <map>
+#include <vector>
+
+class TxOrphanage;
+namespace node {
+static constexpr bool DEFAULT_ENABLE_PACKAGE_RELAY{false};
+
+class TxPackageTracker {
+    class Impl;
+    const std::unique_ptr<Impl> m_impl;
+
+public:
+    explicit TxPackageTracker();
+    ~TxPackageTracker();
+
+    bool OrphanageAddTx(const CTransactionRef& tx, NodeId peer);
+
+    /** Check if we already have an orphan transaction (by txid or wtxid) */
+    bool OrphanageHaveTx(const GenTxid& gtxid);
+
+    /** Extract a transaction from a peer's work set
+     *  Returns nullptr if there are no transactions to work on.
+     *  Otherwise returns the transaction reference, and removes
+     *  it from the work set.
+     */
+    CTransactionRef GetTxToReconsider(NodeId peer);
+
+    int EraseOrphanTx(const uint256& wtxid);
+
+    /** Erase all orphans announced by a peer (eg, after that peer disconnects) */
+    void DisconnectedPeer(NodeId peer);
+
+    /** Erase all orphans included in or invalidated by a new block */
+    void BlockConnected(const CBlock& block);
+
+    void LimitOrphans(unsigned int max_orphans);
+
+    void AddChildrenToWorkSet(const CTransaction& tx);
+
+    /** Does this peer have any orphans to validate? */
+    bool HaveTxToReconsider(NodeId peer);
+
+    /** Return how many entries exist in the orphange */
+    size_t OrphanageSize();
+};
+} // namespace node
+#endif // BITCOIN_NODE_TXPACKAGETRACKER_H
