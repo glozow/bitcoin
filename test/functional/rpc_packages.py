de389697c8f4df82877eed907434911d850c936c
glozow <gloriajzhao@gmail.com>
2022-09-28 14:07:56 +0100

    [rpc] return effective feerate in testmempoolaccept and submitpackage

--- test/functional/rpc_packages.py
+++ test/functional/rpc_packages.py
@@ -358,13 +358,9 @@ class RPCPackagesTest(BitcoinTestFramework):
             wtxid = tx.getwtxid()
             assert wtxid in submitpackage_result["tx-results"]
             tx_result = submitpackage_result["tx-results"][wtxid]
-            assert_equal(tx_result, {
-                "txid": tx.rehash(),
-                "vsize": tx.get_vsize(),
-                "fees": {
-                    "base": DEFAULT_FEE,
-                }
-            })
+            assert_equal(tx_result["txid"], tx.rehash())
+            assert_equal(tx_result["vsize"], tx.get_vsize())
+            assert_equal(tx_result["fees"]["base"], DEFAULT_FEE)
 
         # submitpackage result should be consistent with testmempoolaccept and getmempoolentry
         self.assert_equal_package_results(node, testmempoolaccept_result, submitpackage_result)
@@ -403,6 +399,9 @@ class RPCPackagesTest(BitcoinTestFramework):
         assert_equal(rich_parent_result["fees"]["base"], DEFAULT_FEE)
         assert_equal(poor_parent_result["fees"]["base"], 0)
         assert_equal(child_result["fees"]["base"], DEFAULT_FEE)
+        assert "effective-feerate" not in rich_parent_result["fees"]
+        assert_fee_amount(DEFAULT_FEE, tx_poor.get_vsize() + tx_child.get_vsize(), poor_parent_result["fees"]["effective-feerate"])
+        assert_fee_amount(DEFAULT_FEE, tx_poor.get_vsize() + tx_child.get_vsize(), child_result["fees"]["effective-feerate"])
         # Package feerate is calculated for the remaining transactions after deduplication and
         # individual submission. Since this package had a 0-fee parent, package feerate must have
         # been used and returned.
