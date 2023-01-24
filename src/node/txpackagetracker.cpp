e3ce04cefde136e391c91284a85531aefaa22aec
glozow <gloriajzhao@gmail.com>
2023-01-24 13:12:56 +0000

    [txpackagetracker] add orphan resolution tracker

--- src/node/txpackagetracker.cpp
+++ src/node/txpackagetracker.cpp
@@ -4,18 +4,149 @@
 
 #include <node/txpackagetracker.h>
 
+#include <common/bloom.h>
+#include <logging.h>
+#include <txorphanage.h>
+#include <txrequest.h>
+#include <util/hasher.h>
+
 namespace node {
     /** How long to wait before requesting orphan ancpkginfo/parents from an additional peer.
      * Same as GETDATA_TX_INTERVAL. */
     static constexpr auto ORPHAN_ANCESTOR_GETDATA_INTERVAL{60s};
+
 class TxPackageTracker::Impl {
     /** Manages unvalidated tx data (orphan transactions for which we are downloading ancestors). */
     TxOrphanage& orphanage_ref;
+
+    mutable Mutex m_mutex;
+
+    /** Tracks orphans for which we need to request ancestor information. All hashes stored are
+     * wtxids, i.e., the wtxid of the orphan. However, the is_wtxid field is used to indicate
+     * whether we would request the ancestor information by wtxid (via package relay) or by txid
+     * (via prevouts of the missing inputs). */
+    TxRequestTracker orphan_request_tracker GUARDED_BY(m_mutex);
+
 public:
     Impl(TxOrphanage& orphanage) : orphanage_ref{orphanage} {}
+    void DisconnectedPeer(NodeId nodeid) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    {
+        AssertLockNotHeld(m_mutex);
+        LOCK(m_mutex);
+        orphan_request_tracker.DisconnectedPeer(nodeid);
+    }
+    void AddOrphanTx(NodeId nodeid, const uint256& wtxid, bool is_preferred, std::chrono::microseconds reqtime)
+        EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    {
+        AssertLockNotHeld(m_mutex);
+        LOCK(m_mutex);
+        // Even though this stores the orphan wtxid, is_wtxid=false because we will be requesting the parents via txid.
+        orphan_request_tracker.ReceivedInv(nodeid, GenTxid::Txid(wtxid), is_preferred, reqtime);
+    }
+    size_t CountInFlight(NodeId nodeid) const EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    {
+        AssertLockNotHeld(m_mutex);
+        LOCK(m_mutex);
+        auto count{orphan_request_tracker.CountInFlight(nodeid)};
+        return count;
+    }
+    size_t Count(NodeId nodeid) const EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    {
+        AssertLockNotHeld(m_mutex);
+        LOCK(m_mutex);
+        auto count{orphan_request_tracker.Count(nodeid)};
+        return count;
+    }
+
+    std::vector<GenTxid> GetOrphanRequests(NodeId nodeid, std::chrono::microseconds current_time)
+        EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    {
+        AssertLockNotHeld(m_mutex);
+        LOCK(m_mutex);
+        std::vector<std::pair<NodeId, GenTxid>> expired;
+        auto tracker_requestable = orphan_request_tracker.GetRequestable(nodeid, current_time, &expired);
+        for (const auto& entry : expired) {
+            LogPrint(BCLog::TXPACKAGES, "\nTimeout of inflight %s %s from peer=%d\n", entry.second.IsWtxid() ? "ancpkginfo" : "orphan parent",
+                entry.second.GetHash().ToString(), entry.first);
+        }
+        std::vector<GenTxid> results;
+        for (const auto& gtxid : tracker_requestable) {
+            LogPrint(BCLog::TXPACKAGES, "\nResolving orphan %s, requesting by txids of parents from peer=%d\n", gtxid.GetHash().ToString(), nodeid);
+            const auto ptx = orphanage_ref.GetTx(gtxid.GetHash());
+            if (!ptx) {
+                // We can't request ancpkginfo and we have no way of knowing what the missing
+                // parents are (it could also be that the orphan has already been resolved).
+                // Give up.
+                orphan_request_tracker.ForgetTxHash(gtxid.GetHash());
+                LogPrint(BCLog::TXPACKAGES, "\nForgetting orphan %s from peer=%d\n", gtxid.GetHash().ToString(), nodeid);
+                continue;
+            }
+            // Add the orphan's parents. Net processing will filter out what we already have.
+            // Deduplicate parent txids, so that we don't have to loop over
+            // the same parent txid more than once down below.
+            std::vector<uint256> unique_parents;
+            unique_parents.reserve(ptx->vin.size());
+            for (const auto& txin : ptx->vin) {
+                // We start with all parents, and then remove duplicates below.
+                unique_parents.push_back(txin.prevout.hash);
+            }
+            std::sort(unique_parents.begin(), unique_parents.end());
+            unique_parents.erase(std::unique(unique_parents.begin(), unique_parents.end()), unique_parents.end());
+            for (const auto& txid : unique_parents) {
+                results.emplace_back(GenTxid::Txid(txid));
+            }
+            // Mark the orphan as requested
+            orphan_request_tracker.RequestedTx(nodeid, gtxid.GetHash(), current_time + ORPHAN_ANCESTOR_GETDATA_INTERVAL);
+        }
+        if (!results.empty()) LogPrint(BCLog::TXPACKAGES, "\nRequesting %u items from peer=%d\n", results.size(), nodeid);
+        return results;
+    }
+    void FinalizeTransactions(const std::set<uint256>& valid, const std::set<uint256>& invalid) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    {
+        AssertLockNotHeld(m_mutex);
+        LOCK(m_mutex);
+        for (const auto& wtxid : valid) {
+            orphan_request_tracker.ForgetTxHash(wtxid);
+        }
+        for (const auto& wtxid : invalid) {
+            orphan_request_tracker.ForgetTxHash(wtxid);
+        }
+    }
+    void HandleNewBlock(const CBlock& block) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    {
+        AssertLockNotHeld(m_mutex);
+        const auto wtxids_erased{orphanage_ref.EraseForBlock(block)};
+        std::set<uint256> block_wtxids;
+        std::set<uint256> conflicted_wtxids;
+        for (const CTransactionRef& ptx : block.vtx) {
+            block_wtxids.insert(ptx->GetWitnessHash());
+        }
+        for (const auto& wtxid : wtxids_erased) {
+            if (block_wtxids.count(wtxid) == 0) {
+                conflicted_wtxids.insert(wtxid);
+            }
+        }
+        FinalizeTransactions(block_wtxids, conflicted_wtxids);
+    }
 };
 
 TxPackageTracker::TxPackageTracker(TxOrphanage& orphanage) : m_impl{std::make_unique<TxPackageTracker::Impl>(orphanage)} {}
 TxPackageTracker::~TxPackageTracker() = default;
 
+void TxPackageTracker::DisconnectedPeer(NodeId nodeid) { m_impl->DisconnectedPeer(nodeid); }
+
+void TxPackageTracker::AddOrphanTx(NodeId nodeid, const uint256& wtxid, bool is_preferred, std::chrono::microseconds reqtime)
+{
+    m_impl->AddOrphanTx(nodeid, wtxid, is_preferred, reqtime);
+}
+size_t TxPackageTracker::CountInFlight(NodeId nodeid) const { return m_impl->CountInFlight(nodeid); }
+size_t TxPackageTracker::Count(NodeId nodeid) const { return m_impl->Count(nodeid); }
+std::vector<GenTxid> TxPackageTracker::GetOrphanRequests(NodeId nodeid, std::chrono::microseconds current_time) {
+    return m_impl->GetOrphanRequests(nodeid, current_time);
+}
+void TxPackageTracker::FinalizeTransactions(const std::set<uint256>& valid, const std::set<uint256>& invalid)
+{
+    m_impl->FinalizeTransactions(valid, invalid);
+}
+void TxPackageTracker::HandleNewBlock(const CBlock& block) { m_impl->HandleNewBlock(block); }
 } // namespace node
