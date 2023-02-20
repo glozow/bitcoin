4245454b305f9d6160683b5a8330d72a23fc68a8
glozow <gloriajzhao@gmail.com>
2023-02-20 09:59:14 +0000

    [orphanage] return erased wtxids from EraseForBlock

--- src/txorphanage.cpp
+++ src/txorphanage.cpp
@@ -213,7 +213,7 @@ bool TxOrphanage::HaveTxToReconsider(NodeId peer)
     return false;
 }
 
-void TxOrphanage::EraseForBlock(const CBlock& block)
+std::vector<uint256> TxOrphanage::EraseForBlock(const CBlock& block)
 {
     LOCK(m_mutex);
 
@@ -242,4 +242,5 @@ void TxOrphanage::EraseForBlock(const CBlock& block)
         }
         LogPrint(BCLog::MEMPOOL, "Erased %d orphan tx included or conflicted by block\n", nErased);
     }
+    return vOrphanErase;
 }
