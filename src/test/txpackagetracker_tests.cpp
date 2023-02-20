c7e60cf5e05a41eea624a3a0f8dbf9c49ce571ba
glozow <gloriajzhao@gmail.com>
2023-02-20 16:33:09 +0000

    [unit test] Protect orphan, PkgInfoAllowed, ReceivedAncPkgInfo, ReceivedPkgTxns

--- src/test/txpackagetracker_tests.cpp
+++ src/test/txpackagetracker_tests.cpp
@@ -30,6 +30,34 @@ static inline CTransactionRef make_tx(const std::vector<COutPoint>& inputs, size
 }
 
 BOOST_FIXTURE_TEST_SUITE(txpackagetracker_tests, BasicTestingSetup)
+BOOST_AUTO_TEST_CASE(orphan_protection)
+{
+    FastRandomContext det_rand{true};
+    TxOrphanage orphanage;
+
+    std::vector<CTransactionRef> protected_txns;
+    for (auto i{0}; i < 100; ++i) {
+        const auto tx{make_tx({COutPoint{det_rand.rand256(), 0}}, 1)};
+        orphanage.AddTx(tx, i);
+        if (i % 10 == 0) {
+            orphanage.ProtectOrphan(tx->GetWitnessHash());
+            protected_txns.push_back(tx);
+        }
+    }
+    BOOST_CHECK_EQUAL(orphanage.Size(), 100);
+    BOOST_CHECK_EQUAL(orphanage.NumProtected(), 10);
+    orphanage.LimitOrphans(/*max_orphans=*/5);
+    BOOST_CHECK_EQUAL(orphanage.Size(), 15);
+    BOOST_CHECK_EQUAL(orphanage.NumProtected(), 10);
+    for (const auto& tx : protected_txns) {
+        BOOST_CHECK(orphanage.HaveTx(GenTxid::Wtxid(tx->GetWitnessHash())));
+        BOOST_CHECK(orphanage.HaveTx(GenTxid::Txid(tx->GetHash())));
+        orphanage.UndoProtectOrphan(tx->GetWitnessHash());
+    }
+    BOOST_CHECK_EQUAL(orphanage.NumProtected(), 0);
+    orphanage.LimitOrphans(/*max_orphans=*/5);
+    BOOST_CHECK_EQUAL(orphanage.Size(), 5);
+}
 BOOST_AUTO_TEST_CASE(pkginfo)
 {
     FastRandomContext det_rand{true};
@@ -120,6 +148,76 @@ BOOST_AUTO_TEST_CASE(pkginfo)
     BOOST_CHECK_EQUAL(peer4_requests_later.size(), 2);
     // This counts as 1 in-flight request
     BOOST_CHECK_EQUAL(tracker.CountInFlight(4), 1);
+
+    // peer0 is allowed to send ancpkginfo for orphan0, but not for any other tx or version
+    BOOST_CHECK(tracker.PkgInfoAllowed(/*nodeid=*/0, orphan0->GetWitnessHash(), node::RECEIVER_INIT_ANCESTOR_PACKAGES));
+    BOOST_CHECK(!tracker.PkgInfoAllowed(/*nodeid=*/0, orphan0->GetWitnessHash(), unsupported_package_type));
+    BOOST_CHECK(!tracker.PkgInfoAllowed(/*nodeid=*/0, orphan1->GetWitnessHash(), node::RECEIVER_INIT_ANCESTOR_PACKAGES));
+    // No other peers are allowed to send ancpkginfo (they disconnected or aren't registered for it)
+    BOOST_CHECK(!tracker.PkgInfoAllowed(/*nodeid=*/1, orphan1->GetWitnessHash(), node::RECEIVER_INIT_ANCESTOR_PACKAGES));
+    BOOST_CHECK(!tracker.PkgInfoAllowed(/*nodeid=*/2, orphan1->GetWitnessHash(), node::RECEIVER_INIT_ANCESTOR_PACKAGES));
+    BOOST_CHECK(!tracker.PkgInfoAllowed(/*nodeid=*/3, orphan2->GetWitnessHash(), node::RECEIVER_INIT_ANCESTOR_PACKAGES));
+    BOOST_CHECK(!tracker.PkgInfoAllowed(/*nodeid=*/4, orphan2->GetWitnessHash(), node::RECEIVER_INIT_ANCESTOR_PACKAGES));
+
+    // After receiving ancpkginfo, a second ancpkginfo is not allowed
+    const auto missing_wtxid{det_rand.rand256()};
+    std::map<uint256, bool> txdata_status_map;
+    txdata_status_map.emplace(missing_wtxid, true);
+    txdata_status_map.emplace(orphan0->GetWitnessHash(), false);
+    std::vector<uint256> missing_wtxids{missing_wtxid};
+    BOOST_CHECK(!tracker.ReceivedAncPkgInfo(/*nodeid=*/0, orphan0->GetWitnessHash(), txdata_status_map,
+                                            missing_wtxids, GetVirtualTransactionSize(*orphan0), current_time + 100s));
+    BOOST_CHECK(!tracker.PkgInfoAllowed(/*nodeid=*/0, orphan0->GetWitnessHash(), node::RECEIVER_INIT_ANCESTOR_PACKAGES));
+}
+void RegisterPeerForAncestorPackages(node::TxPackageTracker& tracker, NodeId peer)
+{
+    tracker.ReceivedVersion(peer);
+    tracker.ReceivedSendpackages(peer, node::RECEIVER_INIT_ANCESTOR_PACKAGES);
+    tracker.ReceivedVerack(peer, true, true);
+}
+BOOST_AUTO_TEST_CASE(txdata_download)
+{
+    FastRandomContext det_rand{true};
+    node::TxPackageTracker tracker;
+
+    // 2 parents 1 child
+    const auto tx_parent1 = make_tx({COutPoint{det_rand.rand256(), 0}}, 1);
+    const auto tx_parent2 = make_tx({COutPoint{det_rand.rand256(), 0}}, 1);
+    const auto tx_child = make_tx({COutPoint{tx_parent1->GetHash(), 0}, COutPoint{tx_parent2->GetHash(), 0}}, 1);
+    const Package package_2p1c{tx_parent1, tx_parent2, tx_child};
+
+    const auto current_time{GetTime<std::chrono::microseconds>()};
+    {
+    NodeId peer = 0;
+    RegisterPeerForAncestorPackages(tracker, peer);
+
+    tracker.AddOrphanTx(peer, tx_child->GetWitnessHash(), /*is_preferred=*/true, current_time);
+    tracker.OrphanageAddTx(tx_child, peer);
+    const auto requests = tracker.GetOrphanRequests(peer, current_time + 1s);
+    BOOST_CHECK_EQUAL(1, requests.size());
+    BOOST_CHECK(tracker.PkgInfoAllowed(peer, tx_child->GetWitnessHash(), node::RECEIVER_INIT_ANCESTOR_PACKAGES));
+
+    const std::vector<uint256> missing_wtxids{tx_parent1->GetWitnessHash(), tx_parent2->GetWitnessHash()};
+    std::map<uint256, bool> txdata_status_map;
+    txdata_status_map.emplace(tx_parent1->GetWitnessHash(), true);
+    txdata_status_map.emplace(tx_parent2->GetWitnessHash(), true);
+    txdata_status_map.emplace(tx_child->GetWitnessHash(), false);
+    BOOST_CHECK(!tracker.ReceivedAncPkgInfo(peer, tx_child->GetWitnessHash(), txdata_status_map,
+                                            missing_wtxids, GetVirtualTransactionSize(*tx_child),
+                                            current_time + 100s));
+
+    // Nodeid and exact missing transactions must match.
+    BOOST_CHECK(!tracker.ReceivedPkgTxns(peer, {tx_parent1}).has_value());
+    BOOST_CHECK(!tracker.ReceivedPkgTxns(peer, {tx_parent2}).has_value());
+    BOOST_CHECK(!tracker.ReceivedPkgTxns(2, {tx_parent1, tx_parent2}).has_value());
+
+    const auto validate_2p1c = tracker.ReceivedPkgTxns(peer, {tx_parent1, tx_parent2});
+    BOOST_CHECK(validate_2p1c.has_value());
+    BOOST_CHECK_EQUAL(validate_2p1c.value().m_info_provider, peer);
+    BOOST_CHECK_EQUAL(validate_2p1c->m_rep_wtxid, tx_child->GetWitnessHash());
+    BOOST_CHECK_EQUAL(validate_2p1c->m_pkginfo_hash, GetPackageHash(package_2p1c));
+    BOOST_CHECK(validate_2p1c->m_unvalidated_txns == package_2p1c);
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
