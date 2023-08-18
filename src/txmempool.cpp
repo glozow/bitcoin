75bfda29aaa880ac77d2b10d4822af5dd9a315ba
TheCharlatan <seb.kung@gmail.com>
2023-08-18 21:05:56 +0200

    refactor: Make txmempool's UpdateForDescendants static
    The context of this change is an effort towards removing the boost multi
    index includes from the header tree. The goal is that the experimental
    `bitcoin-chainstate` binary can be compiled without requiring boost
    includes.

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -34,6 +34,9 @@
 using MemPoolMultiIndex::CompareTxMemPoolEntryByScore;
 using MemPoolMultiIndex::descendant_score;
 using MemPoolMultiIndex::entry_time;
+using MemPoolMultiIndex::txiter;
+
+typedef std::map<txiter, CTxMemPool::setEntries, CompareIteratorByHash> cacheMap;
 
 bool TestLockPointValidity(CChain& active_chain, const LockPoints& lp)
 {
@@ -52,8 +55,43 @@ bool TestLockPointValidity(CChain& active_chain, const LockPoints& lp)
     return true;
 }
 
-void CTxMemPool::UpdateForDescendants(txiter updateIt, cacheMap& cachedDescendants,
-                                      const std::set<uint256>& setExclude, std::set<uint256>& descendants_to_remove)
+/** UpdateForDescendants is used by UpdateTransactionsFromBlock to update
+ *  the descendants for a single transaction that has been added to the
+ *  mempool but may have child transactions in the mempool, eg during a
+ *  chain reorg.
+ *
+ * @pre CTxMemPoolEntry::m_children is correct for the given tx and all
+ *      descendants.
+ * @pre cachedDescendants is an accurate cache where each entry has all
+ *      descendants of the corresponding key, including those that should
+ *      be removed for violation of ancestor limits.
+ * @post if updateIt has any non-excluded descendants, cachedDescendants has
+ *       a new cache line for updateIt.
+ * @post descendants_to_remove has a new entry for any descendant which exceeded
+ *       ancestor limits relative to updateIt.
+ *
+ * @param[in] updateIt the entry to update for its descendants
+ * @param[in,out] cachedDescendants a cache where each line corresponds to all
+ *     descendants. It will be updated with the descendants of the transaction
+ *     being updated, so that future invocations don't need to walk the same
+ *     transaction again, if encountered in another transaction chain.
+ * @param[in] setExclude the set of descendant transactions in the mempool
+ *     that must not be accounted for (because any descendants in setExclude
+ *     were added to the mempool after the transaction being updated and hence
+ *     their state is already reflected in the parent state).
+ * @param[out] descendants_to_remove Populated with the txids of entries that
+ *     exceed ancestor limits. It's the responsibility of the caller to
+ *     removeRecursive them.
+ * @param[in,out] mapTx a reference to the complete mempool map.
+ * @param[in] limits the mempool limits configuration.
+ */
+static void UpdateForDescendants(
+    txiter updateIt,
+    cacheMap& cachedDescendants,
+    const std::set<uint256>& setExclude,
+    std::set<uint256>& descendants_to_remove,
+    CTxMemPool::indexed_transaction_set& mapTx,
+    const CTxMemPool::Limits& limits)
 {
     CTxMemPoolEntry::Children stageEntries, descendants;
     stageEntries = updateIt->GetMemPoolChildrenConst();
@@ -95,7 +133,7 @@ void CTxMemPool::UpdateForDescendants(txiter updateIt, cacheMap& cachedDescendan
             // Don't directly remove the transaction here -- doing so would
             // invalidate iterators in cachedDescendants. Mark it for removal
             // by inserting into descendants_to_remove.
-            if (descendant.GetCountWithAncestors() > uint64_t(m_limits.ancestor_count) || descendant.GetSizeWithAncestors() > m_limits.ancestor_size_vbytes) {
+            if (descendant.GetCountWithAncestors() > uint64_t(limits.ancestor_count) || descendant.GetSizeWithAncestors() > limits.ancestor_size_vbytes) {
                 descendants_to_remove.insert(descendant.GetTx().GetHash());
             }
         }
@@ -146,7 +184,7 @@ void CTxMemPool::UpdateTransactionsFromBlock(const std::vector<uint256>& vHashes
                 }
             }
         } // release epoch guard for UpdateForDescendants
-        UpdateForDescendants(it, mapMemPoolDescendantsToUpdate, setAlreadyIncluded, descendants_to_remove);
+        UpdateForDescendants(it, mapMemPoolDescendantsToUpdate, setAlreadyIncluded, descendants_to_remove, mapTx, m_limits);
     }
 
     for (const auto& txid : descendants_to_remove) {
