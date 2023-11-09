f8b160a9f46fd3fdbd083b4b56bb28a4c7c40cb7
glozow <gloriajzhao@gmail.com>
2023-11-09 14:44:41 +0000

    [txorphanage] add GetChildren
    Require the orphan fromPeer to be the same; we should not spend one
    peer's ProcessMessages computing things that are the result of another
    peer's orphans.
    This makes things brittle when multiple (honest or not) peers announce
    the same orphan; fixed by supporting multiple announcers.

--- src/txorphanage.h
+++ src/txorphanage.h
@@ -51,6 +51,10 @@ public:
     /** Does this peer have any work to do? */
     bool HaveTxToReconsider(NodeId peer) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);;
 
+    /** Return a child of this parent if this peer provided exactly 1 orphan child of this tx. */
+    std::vector<CTransactionRef> GetChildren(const CTransactionRef& parent, NodeId peer) const
+        EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+
     /** Return how many entries exist in the orphange */
     size_t Size() EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
     {
