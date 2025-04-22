e30ad43ac70f9a343a5291c6a4c88d71798b8bd3
glozow <gloriajzhao@gmail.com>
2025-04-22 16:16:41 -0400

    minigraph

--- src/policy/packages.cpp
+++ src/policy/packages.cpp
@@ -7,12 +7,13 @@
 #include <primitives/transaction.h>
 #include <uint256.h>
 #include <util/check.h>
-
 #include <algorithm>
 #include <cassert>
 #include <iterator>
 #include <memory>
 #include <numeric>
+#include <span>
+#include <utility>
 
 /** IsTopoSortedPackage where a set of txids has been pre-populated. The set is assumed to be correct and
  * is mutated within this function (even if return value is false). */
@@ -167,4 +168,263 @@ uint256 GetPackageHash(const std::vector<CTransactionRef>& transactions)
         hashwriter << wtxid;
     }
     return hashwriter.GetSHA256();
+}
+
+MiniGraph::MiniGraph(const std::vector<CTransactionRef>& txns_in, CFeeRate min_feerate) :
+    m_txns{txns_in},
+    m_min_feerate{min_feerate.GetFee(MAX_PACKAGE_WEIGHT), MAX_PACKAGE_WEIGHT}
+{
+    m_graph = MakeTxGraph(txns_in.size(), MAX_PACKAGE_WEIGHT, 100000);
+}
+
+void MiniGraph::RegisterInfo(const CTransactionRef& tx, CAmount fee, int64_t size, bool needs_package_rbf) {
+    // Changing any transaction's feerate would change the linearization, so we don't permit this action after
+    // Linearize() has been called.
+    if (m_status != Status::REGISTRATION) return;
+
+    TxGraph::Ref ref = m_graph->AddTransaction(FeePerWeight{fee, static_cast<int32_t>(size)});
+    m_info.emplace(tx->GetHash(), Tx(std::move(ref), tx, needs_package_rbf));
+}
+
+void MiniGraph::ScheduleValidation() {
+    if (m_status != Status::REGISTRATION) return;
+
+    // Client-side bug if we forgot to RegisterInfo for some transactions
+    Assume(m_info.size() + m_num_skip_linearization == m_txns.size());
+
+    // Populate TxGraph
+    for (const auto& tx : m_txns) {
+        auto it = m_info.find(tx->GetHash());
+        if (it != m_info.end()) {
+            auto& child = it->second;
+            // Add dependencies with previous transactions. This works because the package is required to be topological.
+            for (const auto& input : child.m_tx->vin) {
+                if (auto it_parent = m_info.find(input.prevout.hash); it_parent != m_info.end()) {
+                    m_graph->AddDependency(it_parent->second, child);
+                }
+            }
+        }
+    }
+
+    // Determine the validation schedule building a "block" out of its transactions.
+    m_builder = m_graph->GetBlockBuilder();
+    m_status = Status::LINEARIZED;
+
+    // Initialize current chunk.
+    UpdateCurrentChunk();
+}
+
+void MiniGraph::UpdateCurrentChunk() {
+    // Shouldn't be called in LINEARIZED_AGAIN because we only allow 1 bonus chunk.
+    if (m_status != Status::LINEARIZED && m_status != Status::LINEARIZED_RECONSIDERABLE) return;
+
+    // If needed, ask the builder for another chunk. If it is nullopt, there is nothing left to validate.
+    if (!m_current_chunk || m_current_chunk->EndOfChunk()) {
+        if (auto builder_chunk{m_builder->GetCurrentChunk()}) {
+            m_current_chunk = ChunkCache(std::move(*builder_chunk));
+        } else {
+            m_current_chunk = std::nullopt;
+            return;
+        }
+    }
+
+    // Now, decide what subset of the chunk to serve. Create a new subchunk starting from the beginning or after the
+    // last subchunk, adding transactions in linearization order until the aggregate feerate meets m_min_feerate or we
+    // reach the end of the chunk.
+    // start_index = 0 + 0 = 0 if just initialized.
+    m_current_chunk->start_index = m_current_chunk->start_index + m_current_chunk->subchunk_len;
+    m_current_chunk->subchunk_len = 0;
+    m_current_chunk->feerate = FeePerWeight{0, 0};
+
+    // Only allow subchunking in the first linearization.
+    bool allow_subchunking{m_status == Status::LINEARIZED || m_status == Status::LINEARIZED_RECONSIDERABLE};
+    while (!m_current_chunk->EndOfChunk()) {
+        auto next = static_cast<const Tx*>(m_current_chunk->chunk.first[m_current_chunk->start_index + m_current_chunk->subchunk_len++]);
+        m_current_chunk->feerate += m_graph->GetIndividualFeerate(*next);
+
+        // Subchunking can prevent package RBF from being applied, so disable it if package RBF is needed.
+        allow_subchunking &= !next->m_needs_package_rbf;
+
+        // Subchunking: use the smallest possible subset of the chunk that meets the minimum feerate.
+        if (allow_subchunking && m_current_chunk->subchunk_len == 1 && m_current_chunk->feerate >= m_min_feerate) {
+            break;
+        }
+    }
+    // If we reach the end of the chunk, serve it even if the feerate is too low. The caller may decide to throw it away.
+}
+
+std::optional<std::pair<std::vector<CTransactionRef>, FeePerWeight>> MiniGraph::GetCurrentSubpackage() const {
+    if (!m_current_chunk) return std::nullopt;
+
+    // We are not doing any calculations here, just translating the cached chunk to a vector of transactions.
+    std::pair<std::vector<CTransactionRef>, FeePerWeight> result;
+    result.first.reserve(m_current_chunk->subchunk_len);
+    for (size_t i = m_current_chunk->start_index; i < m_current_chunk->start_index + m_current_chunk->subchunk_len; ++i) {
+        auto next = static_cast<const Tx*>(m_current_chunk->chunk.first[i]);
+        result.first.emplace_back(next->m_tx);
+    }
+    result.second = m_current_chunk->feerate;
+    return result;
+}
+
+void MiniGraph::MarkValid(const std::vector<CTransactionRef>& subpackage) {
+    switch (m_status) {
+        case Status::REGISTRATION:
+        {
+            m_num_skip_linearization += 1;
+            break;
+        }
+        case Status::LINEARIZED:
+        case Status::LINEARIZED_RECONSIDERABLE:
+        {
+            // First collect all transactions to remove
+            std::vector<std::pair<Txid, TxGraph::Ref*>> to_remove;
+            to_remove.reserve(m_current_chunk->chunk.first.size());
+            for (auto ref : m_current_chunk->chunk.first) {
+                auto tx = static_cast<const Tx*>(ref);
+                to_remove.emplace_back(tx->m_tx->GetHash(), ref);
+            }
+
+            // Stage removal of these transactions
+            if (!m_graph->HaveStaging()) m_graph->StartStaging();
+
+            // Remove transactions and update tracking
+            for (const auto& [txid, ref] : to_remove) {
+                m_to_remove.push_back(txid);
+                m_graph->RemoveTransaction(*ref);
+            }
+
+            m_builder->Include();
+            UpdateCurrentChunk();
+            break;
+        }
+        case Status::LINEARIZED_AGAIN:
+        {
+            m_current_chunk = std::nullopt;
+            // We only allow 1 bonus chunk.
+            m_status = Status::FINAL;
+            break;
+        }
+        case Status::FINAL:
+        {
+            break;
+        }
+    }
+}
+
+void MiniGraph::MarkRejected(const std::vector<CTransactionRef>& subpackage, bool reconsiderable) {
+    switch (m_status) {
+        case Status::REGISTRATION:
+        {
+            m_num_skip_linearization += 1;
+            break;
+        }
+        case Status::LINEARIZED:
+        case Status::LINEARIZED_RECONSIDERABLE:
+        {
+            // Stage removal of these transactions and their descendants.
+            // Reconsiderable transactions are not removed from the graph, as we may want to try them again later.
+            if (reconsiderable) {
+                m_status = Status::LINEARIZED_RECONSIDERABLE;
+            } else {
+                if (!m_graph->HaveStaging()) m_graph->StartStaging();
+
+                const auto& refs_subset = std::span(m_current_chunk->chunk.first).subspan(m_current_chunk->start_index, m_current_chunk->subchunk_len);
+                const auto descendants_union = m_graph->GetDescendantsUnion(refs_subset, TxGraph::Level::TOP);
+                for (auto ref : descendants_union) {
+                    m_to_remove.emplace_back(static_cast<const Tx*>(ref)->m_tx->GetHash());
+                    m_graph->RemoveTransaction(*ref);
+                }
+            }
+
+            // Don't continue with the rest of the chunk (no effect if we aren't subchunking).
+            // Any of the skipped transactions will be reconsidered later.
+            m_current_chunk = std::nullopt;
+
+            // The builder will automatically not serve transactions in the same cluster.
+            m_builder->Skip();
+            UpdateCurrentChunk();
+            break;
+        }
+        case Status::LINEARIZED_AGAIN:
+        {
+            m_current_chunk = std::nullopt;
+            // We only allow 1 bonus chunk.
+            m_status = Status::FINAL;
+            break;
+        }
+        case Status::FINAL:
+        {
+            break;
+        }
+    }
+}
+
+void MiniGraph::ApplyRemovals() {
+    m_builder.reset();
+    if (m_graph->HaveStaging()) m_graph->CommitStaging();
+    m_current_chunk = std::nullopt;
+    Assume(m_graph->GetTransactionCount(TxGraph::Level::MAIN) + m_to_remove.size() == m_info.size());
+    for (const auto& txid : m_to_remove) {
+        Assume(m_info.find(txid) != m_info.end());
+        m_info.erase(txid);
+    }
+    m_to_remove.clear();
+    Assume(m_graph->GetTransactionCount(TxGraph::Level::MAIN) == m_info.size());
+    Assume(!m_graph->HaveStaging());
+}
+
+
+std::optional<std::vector<CTransactionRef>> MiniGraph::MaybeReconsiderBestChunk() {
+    // Most packages (e.g. ones that all succeed) probably don't need a bonus chunk.
+    // Quit now if that's the case; we don't actually need to apply the removals and relinearize.
+    if (m_status != Status::LINEARIZED_RECONSIDERABLE) return std::nullopt;
+
+    // Apply removal of transactions that succeeded in the first round or can't be reconsidered.
+    ApplyRemovals();
+
+    // Using the first linearization, prune transactions from the back whose chunk feerates are below minimum feerate.
+    // We are going to alter the feerates of some transactions, so we need to do this now. If we dip below 2
+    // transactions at any point, we don't need to continue.
+    while (m_graph->GetTransactionCount(TxGraph::Level::MAIN) >= 2) {
+        auto worst_chunk = m_graph->GetWorstMainChunk();
+        if (worst_chunk.second >= m_min_feerate) {
+            break;
+        }
+        for (auto ref : worst_chunk.first) {
+            m_to_remove.emplace_back(static_cast<const Tx*>(ref)->m_tx->GetHash());
+            m_graph->RemoveTransaction(*ref);
+        }
+    }
+    ApplyRemovals();
+
+    // No point in reconsidering if there are fewer than 2 transactions left. You need at least 2 to do package RBF.
+    if (m_graph->GetTransactionCount(TxGraph::Level::MAIN) < 2) {
+        m_status = Status::FINAL;
+        return std::nullopt;
+    }
+
+    // Set the feerates of transactions that need package RBF to 0.
+    for (const auto& [_, tx] : m_info) {
+        if (tx.m_needs_package_rbf) {
+            m_graph->SetTransactionFee(tx, 0);
+        }
+    }
+
+    // Move on to the second linearization.
+    m_graph->DoWork(100000);
+    m_builder = m_graph->GetBlockBuilder();
+
+    // Get the best chunk.
+    if (auto builder_chunk{m_builder->GetCurrentChunk()}) {
+        std::vector<CTransactionRef> result;
+        result.reserve(builder_chunk->first.size());
+        for (auto ref : builder_chunk->first) {
+            const auto tx = static_cast<const Tx*>(ref);
+            Assume(tx->m_tx != nullptr);
+            result.emplace_back(tx->m_tx);
+        }
+        return result;
+    }
+    return std::nullopt;
 }
--- src/policy/packages.h
+++ src/policy/packages.h
@@ -8,10 +8,13 @@
 #include <consensus/consensus.h>
 #include <consensus/validation.h>
 #include <policy/policy.h>
+#include <policy/feerate.h>
 #include <primitives/transaction.h>
+#include <txgraph.h>
 #include <util/hasher.h>
 
 #include <cstdint>
+#include <optional>
 #include <unordered_set>
 #include <vector>
 
@@ -48,6 +51,125 @@ enum class PackageValidationResult {
 /** A package is an ordered list of transactions. The transactions cannot conflict with (spend the
  * same inputs as) one another. */
 using Package = std::vector<CTransactionRef>;
+
+/** Represents a package that is being validated. Tracks the transactions' dependencies, feerate information, and
+ * validation status (some might be skipped because they are already in mempool or invalid and not eligible for
+ * reconsideration). Uses a TxGraph to determine the schedule in which subpackages should be validated, and splits
+ * chunks into subchunks if a subchunk meets the minimum feerate. */
+class MiniGraph
+{
+    // Copy of the transactions provided in the constructor. These must be sorted.
+    const std::vector<CTransactionRef> m_txns;
+
+    // Number of transactions we omit from m_info. These are transactions that are already in mempool or invalid and not eligible for reconsideration.
+    unsigned int m_num_skip_linearization{0};
+
+    // Minimum feerate: when transations meet this feerate, they can be scheduled individually instead of with their
+    // full chunk.
+    const FeePerWeight m_min_feerate;
+
+    enum class Status {
+        REGISTRATION = 0,
+        LINEARIZED,
+        LINEARIZED_RECONSIDERABLE,
+        LINEARIZED_AGAIN,
+        FINAL,
+    };
+
+    Status m_status{Status::REGISTRATION};
+
+    struct Tx : public TxGraph::Ref {
+        CTransactionRef m_tx;
+        // When true, subchunking is not allowed. While the transaction may have a sufficient feerate, it is
+        // insufficient to pay for its replacement(s).
+        bool m_needs_package_rbf{false};
+
+        Tx(TxGraph::Ref&& ref, const CTransactionRef& tx, bool needs_package_rbf)
+            : TxGraph::Ref(std::move(ref)),
+              m_tx{tx},
+              m_needs_package_rbf{needs_package_rbf}
+            {}
+    };
+
+    /** Main data structure. */
+    std::map<Txid, Tx> m_info;
+    std::vector<Txid> m_to_remove;
+
+    std::unique_ptr<TxGraph> m_graph;
+    /** Used to construct validation schedule. */
+    std::unique_ptr<TxGraph::BlockBuilder> m_builder;
+
+    struct ChunkCache {
+        // Full chunk from the builder.
+        std::pair<std::vector<TxGraph::Ref*>, FeePerWeight> chunk;
+
+        size_t start_index{0};
+        size_t subchunk_len{0};
+        FeePerWeight feerate{0, 0};
+
+        // Initialize with a full chunk from the builder.
+        ChunkCache(std::pair<std::vector<TxGraph::Ref*>, FeePerWeight>&& builder_chunk) : chunk{std::move(builder_chunk)} {}
+
+        // Serve current (sub)chunk.
+        std::pair<std::vector<TxGraph::Ref*>, FeePerWeight> GetChunk() const {
+            Assume(start_index + subchunk_len <= chunk.first.size());
+            return {std::vector<TxGraph::Ref*>(chunk.first.begin() + start_index, chunk.first.begin() + start_index + subchunk_len), feerate};
+        }
+
+        // Check whether the current chunk we are serving completes the full chunk.
+        // If so, we need to ask the builder for the next chunk.
+        bool EndOfChunk() const {
+            Assume(start_index + subchunk_len <= chunk.first.size());
+            return start_index + subchunk_len >= chunk.first.size();
+        }
+    };
+
+    // Cache of the current chunk. When nullopt, there are no more chunks to serve.
+    std::optional<ChunkCache> m_current_chunk;
+
+    // Whether the transactions have been linearized.
+    bool Linearized() const { return m_builder != nullptr; }
+
+    // Proceed to the next (sub)chunk. This should be called at the end of ScheduleValidation, MarkValid, and
+    // MarkRejected to update m_current_chunk for the next call to GetCurrentSubpackage.
+    void UpdateCurrentChunk();
+
+    /** Erase all transactions in m_to_remove from m_info. If needed, CommitStaging and reset the builder to apply removals to m_graph.  */
+    void ApplyRemovals();
+
+public:
+    MiniGraph(const std::vector<CTransactionRef>& txns_in, CFeeRate min_feerate);
+
+    /** Register feerate information for a transaction. Overwrites previous data if called
+     * multiple times for the same transaction. Only allowed before linearization. */
+    void RegisterInfo(const CTransactionRef& tx, CAmount fee, int64_t size, bool needs_package_rbf);
+
+    /** Schedule validation of transactions. Each transaction must either be valid, rejected, or
+     * registered. Only allowed if linearization has not happened yet. */
+    void ScheduleValidation();
+
+    /** Get the next subpackage to validate: the smallest (sub)chunk that meets the minimum feerate.
+     * Returns nullopt if there is nothing left to validate. ScheduleValidation must have already been called. This
+     * value is cached; call MarkValid or MarkRejected before the next call to GetCurrentSubpackage, otherwise the same
+     * transactions will be returned. Only allowed after linearization.
+     */
+    std::optional<std::pair<std::vector<CTransactionRef>, FeePerWeight>> GetCurrentSubpackage() const;
+
+    /** Mark that transaction(s) were accepted to mempool (after linearization) or already found there (before
+     * linearization). */
+    void MarkValid(const std::vector<CTransactionRef>& subpackage);
+
+    /** Mark that transaction(s) were rejected from mempool. These transactions will not be included in the
+     * validation schedule. If validation schedule has already been created, these transactions'
+     * cluster will be excluded from further calls to GetCurrentSubpackage. */
+    void MarkRejected(const std::vector<CTransactionRef>& subpackage, bool reconsiderable);
+
+    /** Linearize remaining transactions and determine 1 bonus chunk to validate. */
+    void ReconsiderBestChunk();
+
+    /** After the first round of linearization, maybe get a bonus chunk to validate. */
+    std::optional<std::vector<CTransactionRef>> MaybeReconsiderBestChunk();
+};
 
 class PackageValidationState : public ValidationState<PackageValidationResult> {};
 
