522e4db86f831e238d3ea343c627fe9e1d912e68
glozow <gloriajzhao@gmail.com>
2025-06-17 13:40:06 -0400

    [unit test] TxGraph::Trim

---
+++ src/test/txgraph_tests.cpp
@@ -0,0 +1,116 @@
+// Copyright (c) 2023-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://opensource.org/license/mit/.
+
+#include <txgraph.h>
+
+#include <boost/test/unit_test.hpp>
+
+#include <memory>
+#include <vector>
+
+BOOST_AUTO_TEST_SUITE(txgraph_tests)
+
+BOOST_AUTO_TEST_CASE(txgraph_trim_zigzag)
+{
+    // We will build an oversized "zigzag" or "trellis" graph.
+    const unsigned int max_cluster_count = 50;
+    // First we add the "bottom" transactions, which are in the mempool already.
+    const unsigned int num_bottom_tx = max_cluster_count - 1;
+    // Then add the "top" transactions, which come from disconnected blocks. These are re-added to the mempool and,
+    // while connecting them to the already-in-mempool transactions, we discover the resulting cluster is oversized.
+    const unsigned int num_top_tx = max_cluster_count;
+    const unsigned int total_num_tx = num_top_tx + num_bottom_tx;
+    // Set a very large cluster size limit so that only the count limit is triggered.
+    const uint64_t max_cluster_size = 100'000 * 100;
+
+    // Create a new graph for the test.
+    auto graph = MakeTxGraph(max_cluster_count, max_cluster_size);
+
+    // Add all transactions and store their Refs.
+    std::vector<TxGraph::Ref> refs;
+    refs.reserve(total_num_tx);
+    // The ith bottom transaction is at position `i`.
+    for (unsigned int i = 0; i < num_bottom_tx; ++i) {
+        refs.push_back(graph->AddTransaction({(int64_t)(200-i), 100}));
+    }
+    // The ith top transaction is at position `num_bottom_tx + i`.
+    for (unsigned int i = 0; i < num_top_tx; ++i) {
+        refs.push_back(graph->AddTransaction({(int64_t)(100-i), 100}));
+    }
+
+    // Create the zigzag dependency structure.
+    // Each transaction in the bottom row depends on two adjacent transactions from the top row.
+    for (unsigned int i = 0; i < num_bottom_tx; ++i) {
+        graph->AddDependency(/*parent=*/refs[num_bottom_tx + i], /*child=*/refs[i]);
+        graph->AddDependency(/*parent=*/refs[num_bottom_tx + i + 1], /*child=*/refs[i]);
+    }
+
+    // Check that the graph is now oversized. This also forces the graph to
+    // group clusters and compute the oversized status.
+    BOOST_CHECK_EQUAL(graph->GetTransactionCount(), max_cluster_count * 2 - 1);
+    BOOST_CHECK(graph->IsOversized(false));
+
+    // Call Trim() to remove transactions and bring the cluster back within limits.
+    auto removed_refs = graph->Trim();
+    BOOST_CHECK(!graph->IsOversized(false));
+
+    BOOST_CHECK_EQUAL(removed_refs.size(), total_num_tx - max_cluster_count);
+    BOOST_CHECK_EQUAL(graph->GetTransactionCount(), (max_cluster_count));
+
+    // Only prefix of size max_cluster_count is left. That's the first half of the top and first half of the bottom.
+    for (unsigned int i = 0; i < refs.size(); ++i) {
+        const bool first_half = i < num_bottom_tx / 2 || (i >= num_bottom_tx && i < num_bottom_tx + num_top_tx / 2 + 1);
+        BOOST_CHECK_EQUAL(graph->Exists(refs[i]), first_half);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(txgraph_trim_flower)
+{
+    // We will build an oversized flower-shaped graph: all transactions are spent by 1 descendant.
+    const unsigned int max_cluster_count = 50;
+    // First we add a single "bottom" transaction, which is in the mempool already.
+    // Then add the "top" transactions, which come from disconnected blocks. These are re-added to the mempool and,
+    // while connecting them to the already-in-mempool transactions, we discover the resulting cluster is oversized.
+    const unsigned int num_top_tx = max_cluster_count * 2;
+    const unsigned int total_num_tx = max_cluster_count * 2 + 1;
+
+    // Set a very large cluster size limit so that only the count limit is triggered.
+    const uint64_t max_cluster_size = 100'000 * 100;
+
+    auto graph = MakeTxGraph(max_cluster_count, max_cluster_size);
+
+    // Add all transactions and store their Refs.
+    std::vector<TxGraph::Ref> refs;
+    refs.reserve(total_num_tx);
+
+    // Add all transactions. They are in individual clusters.
+    refs.push_back(graph->AddTransaction({1, 100}));
+    for (unsigned int i = 0; i < num_top_tx; ++i) {
+        refs.push_back(graph->AddTransaction({(int64_t)(500+i), 100}));
+    }
+
+    // The 0th transaction spends all the top transactions.
+    for (unsigned int i = 1; i < total_num_tx; ++i) {
+        graph->AddDependency(/*parent=*/refs[i], /*child=*/refs[0]);
+    }
+
+    // Check that the graph is now oversized. This also forces the graph to
+    // group clusters and compute the oversized status.
+    BOOST_CHECK(graph->IsOversized(false));
+
+    // Call Trim() to remove transactions and bring the cluster back within limits.
+    auto removed_refs = graph->Trim();
+    BOOST_CHECK(!graph->IsOversized(false));
+
+    BOOST_CHECK_EQUAL(removed_refs.size(), max_cluster_count + 1);
+    BOOST_CHECK_EQUAL(graph->GetTransactionCount(), max_cluster_count);
+
+    // Only prefix of size max_cluster_count (last max_cluster_count top transactions) is left.
+    for (unsigned int i = 0; i < refs.size(); ++i) {
+        const bool top_highest_feerate = i > total_num_tx - max_cluster_count - 1;
+        BOOST_CHECK_EQUAL(graph->Exists(refs[i]), top_highest_feerate);
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
