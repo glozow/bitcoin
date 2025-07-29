07778fff3956d87e584d324147b382aa41c03e9a
Suhas Daftuar <sdaftuar@chaincode.com>
2025-07-29 16:16:18 -0400

    Invoke TxGraph::DoWork() at appropriate times

--- src/txmempool.h
+++ src/txmempool.h
@@ -55,6 +55,10 @@ static const uint32_t MEMPOOL_HEIGHT = 0x7FFFFFFF;
  * iterations. */
 static constexpr uint64_t ACCEPTABLE_ITERS = 10'000;
 
+/** How much work we ask TxGraph to do after a mempool change occurs (either
+ * due to a changeset being applied, a new block being found, or a reorg). */
+static constexpr uint64_t POST_CHANGE_WORK = 100 * ACCEPTABLE_ITERS;
+
 /**
  * Test whether the LockPoints height and time are still valid on the current chain
  */
